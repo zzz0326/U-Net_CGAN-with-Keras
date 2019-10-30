10cc94cb6e40727c78fa38929798e51fafda1876
zzz0326 <51013030+zzz0326@users.noreply.github.com>
2019-10-30 21:20:30 +0800

    Add files via upload

--- gan_main.py
+++ gan_main.py
@@ -1,12 +1,12 @@
 from model import *
 from data import *
-
+from keras import objectives
 import datetime
 
 class GAN():
     def __init__(self):
         #初始化图像大小
-        self.channels = 1
+        self.channels = 3
         self.i_height = 256
         self.i_width = 256
         self.i_shape = (self.i_height,self.i_width, self.channels)
@@ -20,7 +20,8 @@ class GAN():
 
         #构建判别器
         optimizer = Adam(0.0002, 0.5)
-        self.discriminator = self.build_discriminator()
+        #self.discriminator = self.build_discriminator()
+        self.discriminator = self.discriminator_image()
         self.discriminator.compile(loss='mse',
                                    optimizer=optimizer,
                                    metrics=['accuracy'])
@@ -44,7 +45,7 @@ class GAN():
         #fake_out是由生成器生成的
         #相当于训练两次 但是给的是两个不同的目标对生成器进行训练
         self.combined.compile(loss=['binary_crossentropy', 'mse'],
-                              loss_weights=[1e-3, 1],
+                              loss_weights=[1e-3, 1e-3],
                               optimizer=optimizer)
     def build_generator(self):
         """U-Net Generator"""
@@ -91,10 +92,84 @@ class GAN():
         output_img = Conv2D(self.channels, kernel_size=4, strides=1, padding='same', activation='tanh')(u7)
 
         return Model(d0, output_img)
+
+    def discriminator_image(self):
+        """
+        discriminator network (patch GAN)
+          stride 2 conv X 2
+            max pooling X 4
+        fully connected X 1
+        """
+        img_size=(256,256)
+        n_filters = 32
+        # set image specifics
+        k = 3  # kernel size
+        s = 2  # stride
+        img_ch = 3  # image channels
+        out_ch = 1  # output channel
+        img_height, img_width = img_size[0], img_size[1]
+        padding = 'same'  # 'valid'
+
+        inputs = Input((img_height, img_width, img_ch))
+
+        conv1 = Conv2D(n_filters, kernel_size=(k, k), strides=(s, s), padding=padding)(inputs)
+        conv1 = BatchNormalization(scale=False, axis=3)(conv1)
+        conv1 = Activation('relu')(conv1)
+        conv1 = Conv2D(n_filters, kernel_size=(k, k), padding=padding)(conv1)
+        conv1 = BatchNormalization(scale=False, axis=3)(conv1)
+        conv1 = Activation('relu')(conv1)
+        pool1 = MaxPooling2D(pool_size=(s, s))(conv1)
+
+        conv2 = Conv2D(2 * n_filters, kernel_size=(k, k), strides=(s, s), padding=padding)(pool1)
+        conv2 = BatchNormalization(scale=False, axis=3)(conv2)
+        conv2 = Activation('relu')(conv2)
+        conv2 = Conv2D(2 * n_filters, kernel_size=(k, k), padding=padding)(conv2)
+        conv2 = BatchNormalization(scale=False, axis=3)(conv2)
+        conv2 = Activation('relu')(conv2)
+        pool2 = MaxPooling2D(pool_size=(s, s))(conv2)
+
+        conv3 = Conv2D(4 * n_filters, kernel_size=(k, k), padding=padding)(pool2)
+        conv3 = BatchNormalization(scale=False, axis=3)(conv3)
+        conv3 = Activation('relu')(conv3)
+        conv3 = Conv2D(4 * n_filters, kernel_size=(k, k), padding=padding)(conv3)
+        conv3 = BatchNormalization(scale=False, axis=3)(conv3)
+        conv3 = Activation('relu')(conv3)
+        pool3 = MaxPooling2D(pool_size=(s, s))(conv3)
+
+        conv4 = Conv2D(8 * n_filters, kernel_size=(k, k), padding=padding)(pool3)
+        conv4 = BatchNormalization(scale=False, axis=3)(conv4)
+        conv4 = Activation('relu')(conv4)
+        conv4 = Conv2D(8 * n_filters, kernel_size=(k, k), padding=padding)(conv4)
+        conv4 = BatchNormalization(scale=False, axis=3)(conv4)
+        conv4 = Activation('relu')(conv4)
+        pool4 = MaxPooling2D(pool_size=(s, s))(conv4)
+
+        conv5 = Conv2D(16 * n_filters, kernel_size=(k, k), padding=padding)(pool4)
+        conv5 = BatchNormalization(scale=False, axis=3)(conv5)
+        conv5 = Activation('relu')(conv5)
+        conv5 = Conv2D(16 * n_filters, kernel_size=(k, k), padding=padding)(conv5)
+        conv5 = BatchNormalization(scale=False, axis=3)(conv5)
+        conv5 = Activation('relu')(conv5)
+
+        gap = GlobalAveragePooling2D()(conv5)
+        outputs = Dense(1, activation='sigmoid')(gap)
+
+        d = Model(inputs, outputs)
+        '''
+        def d_loss(y_true, y_pred):
+            L = objectives.binary_crossentropy(K.batch_flatten(y_true),
+                                               K.batch_flatten(y_pred))
+            #         L = objectives.mean_squared_error(K.batch_flatten(y_true),
+            #                                            K.batch_flatten(y_pred))
+            return L
+
+        d.compile(optimizer=Adam(lr=init_lr, beta_1=0.5), loss=d_loss, metrics=['accuracy'])
+        '''
+        return d
     def build_discriminator(self):
 
         ndf = 64
-        input_size = (256, 256, 1)
+        input_size = (256, 256, 3)
         n_layers, use_sigmoid = 3, False
         inputs = Input(shape=input_size)
 
@@ -130,15 +205,16 @@ class GAN():
         start_time = datetime.datetime.now()
 
         for epoch in range(epochs):
-            self.generator.load_weights('unet_G_membrane.hdf5')
-            self.discriminator.load_weights('unet_D_membrane.hdf5')
-            gan_testGenerator("data/membrane/train/image")
-            img_real = gan_testGenerator("data/membrane/train/label")
-            img_org = gan_testGenerator("data/membrane/train/image")
+            self.generator.load_weights('unet_G_membrane2.hdf5')
+            self.discriminator.load_weights('unet_D_membrane2.hdf5')
+            #gan_testGenerator("data/membrane/train/paper_image",num_image = 1)
+            img_real = gan_testGenerator("data/membrane/train/paper_label",num_image = 1)
+            img_org = gan_testGenerator("data/membrane/train/paper_image",num_image = 1)
 
             img_fake=[]
             for i in range(batch_size):
-                x = i%30
+                #x = i%30
+                x = 0
                 img = np.reshape(img_org[x], (1,) + img_org[x].shape)
                 img_g = self.generator.predict(img)
                 img_fake.append(img_g)
@@ -148,7 +224,8 @@ class GAN():
 
             #训练判别器
             for i in range(batch_size):
-                x = i % 30
+                #x = i % 30
+                x = 0
                 img = np.reshape(img_real[x],(1,)+img_real[x].shape)
                 #在前面加一个维度
                 d_loss_real = self.discriminator.train_on_batch(img, valid)
@@ -157,27 +234,31 @@ class GAN():
                 d_loss_fake = self.discriminator.train_on_batch(img0, fake)
                 d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
                 #print('d_loss:%f' % (d_loss))
-            self.discriminator.save_weights('unet_D_membrane.hdf5')
+            self.discriminator.save_weights('unet_D_membrane2.hdf5')
             self.discriminator.trainable = False
 
             #训练生成器
             for i in range(batch_size*10):
-                x = i % 30
+                #x = i % 30
+                x = 0
                 img0 = np.reshape(img_real[x], (1,) + img_real[x].shape)
                 img1 = np.reshape(img_org[x], (1,) + img_org[x].shape)
                 g_loss = self.combined.train_on_batch([img1,img0 ], [valid,img0])
                 elapsed_time = datetime.datetime.now() - start_time
                 print("%d %d time: %s" % (i, epoch, elapsed_time))
                 #print("  g_loss:%f\n" % (g_loss))
-            self.generator.save_weights('unet_G_membrane.hdf5')
+            self.generator.save_weights('unet_G_membrane2.hdf5')
 
             #输出信息
-
+    def LoadWeight(self):
+        self.generator.load_weights('unet_G_membrane2.hdf5')
+        self.discriminator.load_weights('unet_D_membrane2.hdf5')
 
 
 if __name__ == '__main__':
     gan =  GAN()
-    gan.train(epochs=1, batch_size=300)
-    testGene1 = testGenerator("data/membrane/1")
-    results = gan.generator.predict_generator(testGene1, 30, verbose=1)
-    saveResult("data/membrane/gan", results)
+    gan.LoadWeight()
+    #gan.train(epochs=5, batch_size=1)
+    testGene1 = testGenerator("data/membrane/train/paper_image")
+    results = gan.generator.predict_generator(testGene1, 1, verbose=1)
+    saveResult("data/membrane/train/out", results)
